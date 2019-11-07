c22787490a55e99005448e1db9e7d33db378a7f5
cabogabo <gabo.alejandro.huitron@gmail.com>
2019-11-07 10:22:28 -0600

    Corregidas tus mmdas :v

--- index.js
+++ index.js
@@ -1,51 +1,91 @@
-const criteriaA2 = require('./classifiers/criteriaA2');
-const criteriaA3 = require('./classifiers/criteriaA3');
-const criteriaA4 = require('./classifiers/criteriaA4');
-const criteriaA6 = require('./classifiers/criteriaA6');
-const criteriaA7 = require('./classifiers/criteriaA7');
-const criteriaA8 = require('./classifiers/criteriaA8');
-const criteriaA9 = require('./classifiers/criteriaA9');
-const criteriaB1 = require('./classifiers/criteriaB1');
-const criteriaB4 = require('./classifiers/criteriaB4');
-const criteriaB6 = require('./classifiers/criteriaB6');
-const criteriaC1 = require('./classifiers/criteriaC1');
+const criteriaA2 = require("./classifiers/criteriaA2");
+const criteriaA3 = require("./classifiers/criteriaA3");
+const criteriaA4 = require("./classifiers/criteriaA4");
+const criteriaA6 = require("./classifiers/criteriaA6");
+const criteriaA7 = require("./classifiers/criteriaA7");
+const criteriaA8 = require("./classifiers/criteriaA8");
+const criteriaA9 = require("./classifiers/criteriaA9");
+const criteriaB1 = require("./classifiers/criteriaB1");
+const criteriaB4 = require("./classifiers/criteriaB4");
+const criteriaB6 = require("./classifiers/criteriaB6");
+const criteriaC1 = require("./classifiers/criteriaC1");
 
 async function main(posts) {
-    let classifiers = await Promise.all([criteriaA2.train(), criteriaA3.train(), criteriaA4.train(), criteriaA6.train(), criteriaA7.train(),
-        criteriaA8.train(), criteriaA9.train(), criteriaB1.train(), criteriaB4.train(), criteriaB6.train(), criteriaC1.train()
-    ]);
+  let classifiers = await Promise.all([
+    criteriaA2.train(),
+    criteriaA3.train(),
+    criteriaA4.train(),
+    criteriaA6.train(),
+    criteriaA7.train(),
+    criteriaA8.train(),
+    criteriaA9.train(),
+    criteriaB1.train(),
+    criteriaB4.train(),
+    criteriaB6.train(),
+    criteriaC1.train()
+  ]);
 
-    let tags = ['perdidaInteres', 'modPeso', 'insomnio', 'fatiga', 'inutilidad', 'disminucion', 'p_muerte', 'malestar', 'bajaAutoestima', 'desesperanza', 'consumoAfeccion'];
-    let ocurrencias = {
-        perdidaInteres: 0,
-        modPeso: 0,
-        insomnio: 0,
-        fatiga: 0,
-        inutilidad: 0,
-        disminucion: 0,
-        p_muerte: 0,
-        malestar: 0,
-        bajaAutoestima: 0,
-        desesperanza: 0,
-        consumoAfeccion: 0,
-    };
-    let i = 0;
-    for (const classifier of classifiers) {
-        posts.forEach(post => {
-            const result = classifier.getBestClassification(post.content);
-            if (result.label === tags[i] && result.value > 0.95) {
-                //console.log(texto);
-                //console.log(result);
-                ocurrencias[`${result.label}`]++;
-            }
-        });
+  let tags = [
+    "perdidaInteres",
+    "modPeso",
+    "insomnio",
+    "fatiga",
+    "inutilidad",
+    "disminucion",
+    "p_muerte",
+    "malestar",
+    "bajaAutoestima",
+    "desesperanza",
+    "consumoAfeccion"
+  ];
 
-        i++;
-    }
+  let ocurrencias = {
+    perdidaInteres: 0,
+    modPeso: 0,
+    insomnio: 0,
+    fatiga: 0,
+    inutilidad: 0,
+    disminucion: 0,
+    p_muerte: 0,
+    malestar: 0,
+    bajaAutoestima: 0,
+    desesperanza: 0,
+    consumoAfeccion: 0
+  };
 
-    return ocurrencias;
+  let postOcurrencias = {
+    perdidaInteres: [],
+    modPeso: [],
+    insomnio: [],
+    fatiga: [],
+    inutilidad: [],
+    disminucion: [],
+    p_muerte: [],
+    malestar: [],
+    bajaAutoestima: [],
+    desesperanza: [],
+    consumoAfeccion: []
+  };
+
+  let i = 0;
+
+  for (const classifier of classifiers) {
+    posts.forEach(post => {
+      const result = classifier.getBestClassification(post.content);
+      if (result.label === tags[i] && result.value > 0.95) {
+        //console.log(texto);
+        //console.log(result);
+        ocurrencias[`${result.label}`]++;
+        postOcurrencias[`${result.label}`].push(post.id);
+      }
+    });
+
+    i++;
+  }
+
+  return [ocurrencias, postOcurrencias];
 }
 
 module.exports = {
-    main
-}
+  main
+};
